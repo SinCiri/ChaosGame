b8a3eec8663643044ce8dac5a56134fdc17b7d18
Melody Phillips <melmelodica@gmail.com>
2024-04-14 14:15:44 -0700

    Sound effects and music added

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -14,6 +14,37 @@ int main() {
 	vector<Vector2f> points;
 
 
+	//set up sound effects
+	sf::Sound menuSound;
+	sf::Sound selectionSound;
+	sf::Sound waterDrop;
+
+	sf::SoundBuffer buffer3;
+	buffer3.loadFromFile("water drop.wav");
+	waterDrop.setBuffer(buffer3);
+
+	sf::SoundBuffer buffer1;
+	buffer1.loadFromFile("menuSound.wav");
+	menuSound.setBuffer(buffer1);
+
+	sf::SoundBuffer buffer2;
+	buffer2.loadFromFile("selectionSound.wav");
+	selectionSound.setBuffer(buffer2);
+
+	//set up music
+	sf::Music menuMusic;
+	if (!menuMusic.openFromFile("menuMusic.wav")) { cout << "not loading menu music"; }
+	menuMusic.setVolume(45.f);
+	menuMusic.setLoop(true);
+
+	sf::Music gameMusic;
+	if(!gameMusic.openFromFile("gameMusic.ogg")) { cout << "not loading game music"; }
+	gameMusic.setVolume(45.f);
+	gameMusic.setLoop(true);
+
+	menuMusic.play();
+
+
 	while (MENU.isOpen()) {
 		Event event;
 		//render mainMenu and ask for event
@@ -24,21 +55,26 @@ int main() {
 			}
 			//if key up is pressed move menu option up.
 			if (event.type == Event::KeyPressed) {
+
 				if (event.key.code == Keyboard::Up) {
+					menuSound.play();
 					mainMenu.moveUp();
 					break;
 				}
 				//if key down is pressed move down
 				if (event.key.code == Keyboard::Down) {
+					menuSound.play();
 					mainMenu.moveDown();
 					break;
 				}
 				//if escape is pressed quit game
 				if (event.key.code == Keyboard::Escape) {
+					menuSound.play();
 					MENU.close();
 				}
 				//if enter is selected render play window
 				if (event.key.code == Keyboard::Return) {
+					selectionSound.play();													//selection sound
 					//render windows when play presses enter
 					RenderWindow play(vm, "Chaos Game");
 					RenderWindow settings(vm, "Settings");
@@ -46,10 +82,13 @@ int main() {
 					int x = mainMenu.MainMenuSelected();
 					//if play is selected start the game loop
 					if (x == 0) {
+						menuMusic.pause();
+						gameMusic.play();
 						///////////////////////////////////////////////////////////////////////////////
 						//							main game loop									 //
 						///////////////////////////////////////////////////////////////////////////////
 						while (play.isOpen()) {
+							
 							//look for new event?
 							Event aevent;
 							while (play.pollEvent(aevent)) {
@@ -69,11 +108,16 @@ int main() {
 										cout << "Left click registered at: " << endl;
 										cout << "mouse x: " << aevent.mouseButton.x << endl;
 										cout << "mouse y: " << aevent.mouseButton.y << endl;
+										
 
 										if (vertices.size() < 3) {
+											
+											waterDrop.play();
 											vertices.push_back(Vector2f(aevent.mouseButton.x, aevent.mouseButton.y));
 										}
 										else if (points.size() == 0) {
+											
+											waterDrop.play();
 											//register forth click to begin
 											points.push_back(Vector2f(aevent.mouseButton.x, aevent.mouseButton.y));
 										}
@@ -116,10 +160,11 @@ int main() {
 								}
 								
 								//display
-								
 								play.display();
 							
 						}
+						gameMusic.pause();
+						menuMusic.play();
 					}
 					//if x == 1
 					//start the settings menu
@@ -137,22 +182,27 @@ int main() {
 								}
 								if (aevent.type == Event::KeyPressed) {
 									if (aevent.key.code == Keyboard::Left) {
+										menuSound.play();
 										settingsMenu.moveLeft();
 										break;
 									}
 									if (aevent.key.code == Keyboard::Right) {
+										menuSound.play();
 										settingsMenu.moveRight();
 										break;
 									}
 									if (aevent.key.code == Keyboard::Up) {
+										menuSound.play();
 										settingsMenu.up();
 										break;
 									}
 									if (aevent.key.code == Keyboard::Down) {
+										menuSound.play();
 										settingsMenu.down();
 										break;
 									}
 									if (aevent.key.code == Keyboard::Return) {
+										selectionSound.play();
 										cout << "Added Setting" << endl;
 										int x = settingsMenu.getX();
 										int y = settingsMenu.getY();
@@ -195,6 +245,7 @@ int main() {
 
 				}
 			}
+
 			MENU.clear();
 			mainMenu.draw(MENU);
 			MENU.display();
